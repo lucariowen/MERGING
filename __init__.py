0434b9b74617e1d220121379527782ceca0c8156
lucariowen <lucariowen@gmail.com>
2023-02-13 18:14:24 +0800

    a

--- __init__.py
+++ __init__.py
@@ -32,16 +32,20 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     points = db.Column(db.Integer)
     ordered = db.Column(db.String)
+    contact = db.Column(db.String)
+    address = db.Column(db.String)
 
-    def __init__(self, username, password, email=None, points=0, ordered=None):
+    def __init__(self, username, password, email=None, points=0, ordered=None, contact=None, address=None):
         self.username = username
         self.password = password
         self.email = email
         self.points = points
         self.ordered = ordered
+        self.contact = contact
+        self.address = address
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.points}', '{self.ordered}')"
+        return f"User('{self.username}', '{self.email}', '{self.points}', '{self.ordered}'), '{self.contact}', '{self.address}'))"
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
